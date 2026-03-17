b8e70cb0edfaf5b5901e7826ddfbcc47969a22a7
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2026-03-17 20:14:08 +0900

    input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.Container}>
       <Text style={styles.title}>Todo App</Text>
+      <TextInput placeholder="タスクを入力" style={styles.input} />
     </View>
   );
 }
@@ -12,11 +13,19 @@ const styles = StyleSheet.create({
   Container: {
     flex: 1,
     padding: 40,
+    backgroundColor: "white",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
     marginTop: 40,
-    color: "white",
+    color: "black",
+  },
+  input: {
+    borderColor: "#000327",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 6,
+    borderWidth: 2,
   },
 });
